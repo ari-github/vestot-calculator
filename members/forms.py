785112e4dc26a9ba93054bbd816aeb1e8457c961
Ari <ayhavlin@gmail.com>
2021-04-12 16:15:46 +0300

    added edit proile

--- members/forms.py
+++ members/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -6,9 +6,6 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'password1' ,'password2')
-        # widgets = {
-            # 'username': forms.TextInput(attrs={'class': 'form-control'})
-        # }
 
     def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
@@ -18,12 +15,21 @@ class CustomUserCreationForm(UserCreationForm):
         # self.fields['password1'].widget.attrs['placeholder'] = 'Enter a Password'
         # self.fields['password1'].label=''
         # self.fields['password1'].help_text='<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>our password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
-
         self.fields['password2'].widget.attrs['class'] = 'form-control'
-        # self.fields['password2'].widget.attrs['placeholder'] = 'Verify the Password'
-        # self.fields['password2'].label=''
-        # self.fields['password2'].help_text='<div class="form-text text-muted"><small>Enter the same password as before, for verification.</small></div>'
 
+
+class EditProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email',)
+
+    def __init__(self, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['first_name'].widget.attrs['class'] = 'form-control'
+        self.fields['last_name'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        
 
 class CustomAuthenticationForm(AuthenticationForm):
     class Meta:
@@ -34,4 +40,21 @@ class CustomAuthenticationForm(AuthenticationForm):
         super(CustomAuthenticationForm, self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['password'].widget.attrs['class'] = 'form-control'
+
+
+class PasswordsChangeForm(PasswordChangeForm):
+    class Meta:
+        model = User
+        fields = ('old_password', 'new_password1', 'new_password2',)
+    
+    def __init__(self, *args, **kwargs):
+        super(PasswordsChangeForm, self).__init__(*args, **kwargs)
+        self.fields['old_password'].widget.attrs['class'] = 'form-control'
+        self.fields['old_password'].widget.attrs['type'] = 'password'
+
+        self.fields['new_password1'].widget.attrs['class'] = 'form-control'
+        self.fields['new_password1'].widget.attrs['type'] = 'password'
+
+        self.fields['new_password2'].widget.attrs['class'] = 'form-control'
+        self.fields['new_password2'].widget.attrs['type'] = 'password'
 
--- members/views.py
+++ members/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
-from .forms import CustomUserCreationForm
+from django.contrib.auth.views import PasswordChangeView
+from .forms import CustomUserCreationForm, EditProfileForm, PasswordsChangeForm
 
 
 class UserRegister(generic.CreateView):
@@ -9,3 +10,19 @@ class UserRegister(generic.CreateView):
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
+class UserEdit(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordsChangeForm
+    template_name = 'registration/change_password.html'
+    success_url = reverse_lazy('password_success')
+
+
+def password_success(request):
+    return render(request, 'registration/password_success.html')
