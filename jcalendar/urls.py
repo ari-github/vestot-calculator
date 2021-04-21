07ddf9febb35cb8a3627d16689e265f445ec115a
Ari <ayhavlin@gmail.com>
2021-04-21 15:39:35 +0300

    set hebrew date format

--- jcalendar/urls.py
+++ jcalendar/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.calendar_home, name='calendar-home'),
-    path('<int:year>/<int:month>', views.month, name='calendar-month'),
+    path('', views.calendar_home_view, name='calendar-home'),
+    path('<int:year>/<int:month>', views.month_view, name='calendar-month'),
 ]
--- jcalendar/views.py
+++ jcalendar/views.py
@@ -1,35 +1,47 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 
 from zmanim.hebrew_calendar.jewish_date import JewishDate
 
-# Create your views here.
-def month(request, year, month):
+from .hebrew_date_formatter import HebrewDateFormatter
+hdf = HebrewDateFormatter()
+
+def month_view(request, year, month):
     jd = JewishDate(year, month, 1)
-    day_list = get_month_data(jd)
 
     return render(request, 'calendar/month.html', {
-        'day_list': day_list,
+        'day_list': get_month_data(jd),
         'prev_month': get_prev(jd),
         'next_month': get_next(jd),
+        'heb_year': hdf.format_hebrew_number(jd.jewish_year),
+        'heb_month': hdf.format_month(jd),
 
         })
 
-
-def calendar_home(request):
+def calendar_home_view(request):
     jd = JewishDate()
     return HttpResponseRedirect(reverse('calendar-month', args=[jd.jewish_year, jd.jewish_month]))
 
+
 def get_month_data(jd):
     jd = JewishDate(jd.jewish_year, jd.jewish_month, 1)
-    day_list = [JewishDate(jd.jewish_year, jd.jewish_month, day+1) for day in range(jd.days_in_jewish_month())]
-    print(day_list[0].day_of_week -1)
-    day_list = ['']* (day_list[0].day_of_week -1) + day_list + ['']* (day_list[-1].day_of_week -1)
-    print(day_list)
-
-    day_list = [day_list[(i-1)*7: i*7] for i in range(1, len(day_list) // 7 + 1)]
-    return day_list
+    day_of_week = jd.day_of_week
+    days_in_month = jd.days_in_jewish_month()
+    
+    data = {}
+    for i in range(1, 36):
+        if i < day_of_week or i > days_in_month:
+            data[i] = {}
+        else:
+            data[i] = {
+                'date': jd,
+                'heb_year': hdf.format_hebrew_number(jd.jewish_year),
+                'heb_month': hdf.format_month(jd),
+                'heb_day': hdf.format_hebrew_number(jd.jewish_day),
+            }
+            jd.forward()
+    return data
 
 def get_prev(jd):
     jd = JewishDate(jd.jewish_year, jd.jewish_month, jd.jewish_day)
